71dd37a459a776f9ee8c254d1263fe3073580e4f
vitalijs.visnakovs <vitalijs.visnakovs@scandiweb.com>
2023-12-04 23:23:16 +0200

    Migration fix

--- migrations/Version20231204212253.php
+++ migrations/Version20231204212253.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace migrations;
+namespace DoctrineMigrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231204205749 extends AbstractMigration
+final class Version20231204212253 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,6 +20,9 @@ final class Version20231204205749 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE account (id INT AUTO_INCREMENT NOT NULL, client_id INT NOT NULL, account_name VARCHAR(255) NOT NULL, funds DOUBLE PRECISION NOT NULL, account_currency VARCHAR(3) NOT NULL, INDEX IDX_7D3656A419EB6921 (client_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE client (id INT AUTO_INCREMENT NOT NULL, client_name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE transaction (id INT AUTO_INCREMENT NOT NULL, sender_account_id INT NOT NULL, receiver_account_id INT NOT NULL, transaction_amount DOUBLE PRECISION NOT NULL, currency VARCHAR(3) NOT NULL, INDEX IDX_723705D1CFEF0177 (sender_account_id), INDEX IDX_723705D1D8CF5973 (receiver_account_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE account ADD CONSTRAINT FK_7D3656A419EB6921 FOREIGN KEY (client_id) REFERENCES client (id)');
         $this->addSql('ALTER TABLE transaction ADD CONSTRAINT FK_723705D1CFEF0177 FOREIGN KEY (sender_account_id) REFERENCES account (id)');
         $this->addSql('ALTER TABLE transaction ADD CONSTRAINT FK_723705D1D8CF5973 FOREIGN KEY (receiver_account_id) REFERENCES account (id)');
@@ -31,5 +34,8 @@ final class Version20231204205749 extends AbstractMigration
         $this->addSql('ALTER TABLE account DROP FOREIGN KEY FK_7D3656A419EB6921');
         $this->addSql('ALTER TABLE transaction DROP FOREIGN KEY FK_723705D1CFEF0177');
         $this->addSql('ALTER TABLE transaction DROP FOREIGN KEY FK_723705D1D8CF5973');
+        $this->addSql('DROP TABLE account');
+        $this->addSql('DROP TABLE client');
+        $this->addSql('DROP TABLE transaction');
     }
 }
