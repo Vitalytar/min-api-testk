be5ae36d7f9b40491db7a027851d797ae24318c8
vitalijs.visnakovs <vitalijs.visnakovs@scandiweb.com>
2023-12-05 23:38:39 +0200

    For test environment adding prevention for entities detach just in case

--- src/Kernel.php
+++ src/Kernel.php
@@ -4,8 +4,22 @@ namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 
-class Kernel extends BaseKernel
+class Kernel extends BaseKernel implements CompilerPassInterface
 {
     use MicroKernelTrait;
+
+    /**
+     * @param ContainerBuilder $container
+     *
+     * @return void
+     */
+    public function process(ContainerBuilder $container): void
+    {
+        if ($this->environment === 'test') {
+            $container->getDefinition('doctrine')->clearTag('kernel.reset');
+        }
+    }
 }
