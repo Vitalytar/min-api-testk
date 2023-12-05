c7ddd64395579ffb0e178567152a10ee08b8fa99
vitalijs.visnakovs <vitalijs.visnakovs@scandiweb.com>
2023-12-05 22:23:51 +0200

    Expanding test cases

--- tests/Entity/AccountEntityTest.php
+++ tests/Entity/AccountEntityTest.php
@@ -2,7 +2,10 @@
 
 namespace App\Tests\Entity;
 
+use App\Entity\Account;
 use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
+use Symfony\Component\Console\Input\StringInput;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 /**
@@ -12,13 +15,45 @@ use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
  */
 final class AccountEntityTest extends ApiTestCase
 {
+    public function setUp(): void
+    {
+        $kernel = AccountEntityTest::createKernel();
+        $kernel->boot();
+
+        $application = new Application($kernel);
+        $application->setAutoExit(false);
+
+        // Load fixtures
+        $input = new StringInput('doctrine:fixtures:load --no-interaction');
+        $application->run($input);
+    }
+
     /**
      * @return void
      * @throws TransportExceptionInterface
      */
-    public function testClientDoesNotExist()
+    public function testAccountDoesNotExist()
     {
-        AccountEntityTest::createClient()->request(method: 'GET', url: 'account/1e');
+        AccountEntityTest::createClient()->request(method: 'GET', url: 'api/account/1e');
         $this->assertResponseStatusCodeSame(404);
+        $this->assertJsonContains([
+            'detail' => 'Not Found',
+        ]);
+    }
+
+    /**
+     * @return void
+     * @throws TransportExceptionInterface
+     */
+    public function testAccountExist()
+    {
+        $client = AccountEntityTest::createClient();
+
+        $repository = $client->getContainer()->get('doctrine')->getRepository(Account::class);
+        $senderAccount = $repository->findOneBy(['account_name' => 'BigCompanyEUR']);
+        $accountId = $senderAccount->getId();
+
+        $client->request(method: 'GET', url: "api/account/$accountId");
+        $this->assertResponseStatusCodeSame(200);
     }
 }
--- tests/Entity/ClientEntityTest.php
+++ tests/Entity/ClientEntityTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Entity;
 
+use App\Entity\Client;
 use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
@@ -18,7 +19,26 @@ final class ClientEntityTest extends ApiTestCase
      */
     public function testClientDoesNotExist()
     {
-        ClientEntityTest::createClient()->request(method: 'GET', url: 'client/1e');
+        ClientEntityTest::createClient()->request(method: 'GET', url: 'api/client/1e');
         $this->assertResponseStatusCodeSame(404);
+        $this->assertJsonContains([
+            'detail' => 'Not Found',
+        ]);
+    }
+
+    /**
+     * @return void
+     * @throws TransportExceptionInterface
+     */
+    public function testAccountExist()
+    {
+        $client = AccountEntityTest::createClient();
+
+        $repository = $client->getContainer()->get('doctrine')->getRepository(Client::class);
+        $senderAccount = $repository->findOneBy(['client_name' => 'BigCompany']);
+        $accountId = $senderAccount->getId();
+
+        $client->request(method: 'GET', url: "api/client/$accountId");
+        $this->assertResponseStatusCodeSame(200);
     }
 }
